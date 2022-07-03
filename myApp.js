456e7bcb3d03e91668e7ceae9078bc434e7646ed
darc-7 <55797130+darc-7@users.noreply.github.com>
2022-07-03 02:19:23 +0000

    Set up MongoDB and connected to a database

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-
+let mongoose = require('mongoose');
 
 let Person;
+
+mongoose.connect('mongodb+srv://admin:mongo123@cluster0.jepmruu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
